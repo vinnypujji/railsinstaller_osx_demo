aa22892ac2ed1a763e3c0a351626f7fa42072203
Vinny Pujji <vpujji@gmail.com>
2012-07-31 02:55:05 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RailsinstallerOsxDemo::Application.config.secret_token = 'a860881cf08796cf5444e8841385d2d3d041b46176acf10242cf652febe4f90317c19d071c2fd76117f6ac4cede41326bc69691a7e165c19041c2cb148b0788a'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+RailsinstallerOsxDemo::Application.config.session_store :cookie_store, key: '_railsinstaller_osx_demo_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# RailsinstallerOsxDemo::Application.config.session_store :active_record_store
